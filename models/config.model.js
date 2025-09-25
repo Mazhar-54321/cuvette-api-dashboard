ff8333fa03031b44ee4326f1b4347529110db37e
Mazhar-54321 <syedmazharali742@gmail.com>
2025-09-25 20:08:58 +0530

    modified config model

--- models/config.model.js
+++ models/config.model.js
@@ -1,20 +1,25 @@
 import { Schema, model } from "mongoose";
+
 const logsConfigSchema = new Schema({
-  apiKey:{type:String,required:true},  
+  apiKey: { type: String, required: true },  
   apiName: { type: String, required: true },
   aliasName: { type: String, required: true },
   startDate: { type: Date, default: Date.now },
-  schedule: new Schema({
+
+  schedule: {
     start: { type: Date, default: Date.now },
     end: {
       type: Date,
-      default: () => new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
+      default: () => new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), 
     },
-  }),
-  requestLimit: new Schema({
+  },
+
+  requestLimit: {
     hour: { type: Number, default: 100 },
     day: { type: Number, default: 2400 },
-  }),
+  },
+
   enabled: { type: Boolean, default: true },
 });
+
 export default model("logsConfig", logsConfigSchema);
