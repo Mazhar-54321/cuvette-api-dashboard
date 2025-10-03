b5c36b929d105213238cf774f006efc1fc5c3033
Mazhar-54321 <syedmazharali742@gmail.com>
2025-10-03 10:54:11 +0530

    config logs

--- controllers/config.controller.js
+++ controllers/config.controller.js
@@ -11,6 +11,7 @@ export const getConfigData = async(req,res,next)=>{
 export const saveConfigData = async(req,res,next)=>{
     try {
         const apiKey = req.body.apiKey;
+        console.log(req.body)
         const data = await ConfigService.saveConfigData(apiKey,req.body);
           res.status(200).json({ data: data, message: "Config data updated successfully" });
       } catch (err) {
